2dbe138f26e5cdf306c2a7c01b704d2f94c228ce
developedbyrd <developedbyrd@gmail.com>
2025-03-27 18:27:06 +0530

    hardcoded env's for testing

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -66,12 +66,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FB_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FB_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FB_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FB_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FB_APP_ID,
+  apiKey: "AIzaSyDiZBqs5jaZrjR0oEmCavdJ2GvVpbksKmU",
+  authDomain: "rapid-routez-69d18.firebaseapp.com",
+  projectId: "rapid-routez-69d18",
+  storageBucket: "rapid-routez-69d18.firebasestorage.app",
+  messagingSenderId: "110054330325",
+  appId: "1:110054330325:web:d2343f7f66af0de87e375c",
 };
 
 const app = initializeApp(firebaseConfig);
